4e17e5214d46f0e66213304ffc19ec881f1ee624
Tetiana Klimenko <ktlimenko@gmail.com>
2023-05-02 12:31:26 +0300

    start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,90 @@
+import { ContactForm } from './ContactForm/ContactForm.jsx';
+import { Filter } from './Filter/Filter.jsx';
+import { ContactList } from './ContactList/ContactList.jsx';
+import { Toaster, toast } from 'react-hot-toast';
+
+import { useDispatch, useSelector } from 'react-redux';
+import { contactCreate, contactRemove } from 'store/contacts/slice.js';
+import { filterChange } from 'store/filter/slice.js';
+
 export const App = () => {
+  const { contacts } = useSelector(state => state.contacts);
+  const { filter } = useSelector(state => state.filter);
+
+  const dispatch = useDispatch();
+
+  const createContact = contact => {
+    if (
+      contacts.some(
+        el => el.name === contact.name && el.number === contact.number
+      )
+    ) {
+      toast.success(`${contact.name} is already in contacts`);
+    } else {
+      dispatch(contactCreate(contact));
+    }
+  };
+
+  const removeContact = contactId => {
+    dispatch(
+      contactRemove(contacts.filter(contact => contact.id !== contactId))
+    );
+  };
+
+  const changeFilter = filter => {
+    dispatch(filterChange(filter));
+  };
+
+  const filteredContacts = () => {
+    if (filter) {
+      const visibleFriends = contacts.filter(el =>
+        el.name.toLowerCase().includes(filter.toLowerCase().trim())
+      );
+      return visibleFriends;
+    } else {
+      return contacts;
+    }
+  };
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <h2>Phone book</h2>
+      <ContactForm onSubmit={createContact} />
+
+      <h2>Contacts</h2>
+      {contacts.length > 1 && (
+        <Filter filter={filter} onChange={changeFilter} />
+      )}
+      {contacts.length > 0 ? (
+        <ContactList contacts={filteredContacts()} onDelete={removeContact} />
+      ) : (
+        <p className="title">No contacts</p>
+      )}
+      <Toaster
+        position="top-left"
+        reverseOrder={false}
+        gutter={8}
+        containerClassName=""
+        containerStyle={{}}
+        toastOptions={{
+          // Define default options
+          className: '',
+          duration: 5000,
+          style: {
+            background: 'rgb(47, 155, 255)',
+            color: '#fff',
+          },
+
+          // Default options for specific types
+          success: {
+            duration: 3000,
+            theme: {
+              primary: 'green',
+              secondary: 'black',
+            },
+          },
+        }}
+      />
     </div>
   );
 };
---
+++ src/components/ContactForm/ContactForm.jsx
@@ -0,0 +1,65 @@
+import { useState } from 'react';
+import { nanoid } from 'nanoid';
+
+import css from 'components/ContactForm/ContactForm.module.css';
+
+export const ContactForm = ({ onSubmit }) => {
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
+
+  const handleChange = ({ target: { name, value } }) => {
+    if (name === 'name') setName(value);
+    else if (name === 'number') setNumber(value);
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    onSubmit({ id: nanoid(), name, number });
+
+    setName('');
+    setNumber('');
+  };
+
+  return (
+    <div>
+      <form className={css.containers} onSubmit={handleSubmit}>
+        <div className={css['phon-container']}>
+          <label htmlFor="exampleInputName" className={css['form-label']}>
+            Name
+          </label>
+          <input
+            type="text"
+            name="name"
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            onChange={handleChange}
+            value={name}
+            placeholder="Jacob Mercer"
+            required
+          />
+        </div>
+
+        <div className={css['phon-container']}>
+          <label htmlFor="exampleInputNumber" className={css['form-label']}>
+            Number
+          </label>
+          <input
+            type="tel"
+            name="number"
+            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+            onChange={handleChange}
+            value={number}
+            placeholder="999-99-99"
+            required
+          />
+        </div>
+
+        <button type="submit" className={css.btn}>
+          Add contact
+        </button>
+      </form>
+    </div>
+  );
+};
